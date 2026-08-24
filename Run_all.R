4288202a7c33262b12fe209d8d344db004f51fc3
bhattaraianisha <bhattaraianisha025@gmail.com>
2026-08-24 15:59:02 +0100

    Fixing end to end pipeline in Run_all.R file
    Fixing the codes in the scripts so that end to end pipeline works without interruption

--- Run_all.R
+++ Run_all.R
@@ -1,7 +1,7 @@
 # Follow and run the full WIMD pipeline in order. Need raw data in data/raw first.
 if (!requireNamespace("rmarkdown", quietly = TRUE)) install.packages("rmarkdown")
 if (!requireNamespace("here", quietly = TRUE)) install.packages("here")
-scripts_in_order <- c(
+Scripts_in_order <- c(
   "01-Validation-LSOA.Rmd",
   "02-explore-rank-data.Rmd",
   "03-explore-score-data.Rmd",
@@ -14,7 +14,7 @@ scripts_in_order <- c(
   "10-case-study-analysis.Rmd",
   "11-Checking-and-visualization-of-data.Rmd"
 )
-for (f in scripts_in_order) {
+for (f in Scripts_in_order) {
   message("==> ", f)
   rmarkdown::render(here::here("Scripts", f), quiet = TRUE)
 }
